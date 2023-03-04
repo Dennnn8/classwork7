150df40bcc5785233fa26a0fce581ed6fee0c0e7
Your Name <you@example.com>
2023-03-04 19:02:59 +0900

    Added new variable to MainActivity.kt

--- app/src/main/java/com/example/classwork7/MainActivity.kt
+++ app/src/main/java/com/example/classwork7/MainActivity.kt
@@ -7,5 +7,13 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val hello = "Hello"
+        val number1 = 100
+        val number2 = 111
+        val number3 = 888
+
+        println(hello)
+        println(number1 + number2 + number3)
     }
 }
